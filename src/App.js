9148a30f750ba0bcd8218959455eb1aa3e2f114c
Dhanasekhar P <dhanasekhar.podigiri@jda.com>
2021-06-22 14:54:46 +0530

    fiels added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h2>TESTING GITHUB PAGES - Test</h2>
+          <h2>TESTING GITHUB PAGES - Tst</h2>
         </p>
         <a
           className="App-link"
